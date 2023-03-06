4ec8ad771708b2f5d6a2e594e022e539d029e5db
ssttkkl <huang.wen.long@hotmail.com>
2023-03-06 16:09:23 +0800

    更新hds插件

--- plugins/nonebot_plugin_hds/__init__.py
+++ plugins/nonebot_plugin_hds/__init__.py
@@ -6,12 +6,9 @@ from nonebot.adapters import Message
 from nonebot.internal.matcher import Matcher
 from nonebot.params import CommandArg
 
-query_hds = on_command("查询活动室状态", aliases={"hds几", "活动室几"}, priority=5)
-update_hds = on_command("活动室", aliases={"hds"}, priority=5)
 
-
-class HDS:
-    count: int  # 应该添加一个每天0：00重置人数为0的操作，但我不知道怎么写
+class Place:
+    count: int
     update_time: datetime
 
     def __init__(self):
@@ -37,32 +34,36 @@ class HDS:
             self.count = 0
 
 
-hds = HDS()
+for place_name, place_shortname in {("活动室", "hds"), ("三元亭", "syt")}:
+    place = Place()
 
+    query_place = on_command(f"查询{place_name}状态",
+                             aliases={f"{place_shortname}几", f"{place_name}几"},
+                             priority=5)
+    update_place = on_command(place_name, aliases={place_shortname}, priority=5)
 
-@query_hds.handle()
-async def query_state(bot: Bot, event: Event):
-    if hds.update_time.date() != datetime.now().date():
-        msg = f"活动室现在0人 (今日未更新数据，更新数据请使用“/活动室3”或“/hds+1”。)"
-    else:
-        msg = f"活动室现在{hds.count}人 (更新于{hds.update_time.strftime('%H:%M')}，更新数据请使用“/活动室3”或“/hds+1”。)"
-    await bot.send(event=event, message=msg)
-
-
-@update_hds.handle()
-async def update_count(bot: Bot, event: Event, matcher: Matcher, args: Message = CommandArg()):
-    plain_text = args.extract_plain_text()
-    try:
-        if plain_text[0] == '+':
-            n1 = int(plain_text[1:])
-            hds.plus(n1)
-        elif plain_text[0] == '-':
-            n1 = int(plain_text[1:])
-            hds.plus(-n1)
+    @query_place.handle()
+    async def query_state(bot: Bot, event: Event):
+        if place.update_time.date() != datetime.now().date():
+            msg = f"{place_name}现在0人 (今日未更新数据，更新数据请使用“/{place_name}3”或“/{place_shortname}+1”。)"
         else:
-            n1 = int(plain_text)
-            hds.set(n1)
-    except ValueError:
-        await matcher.finish(message="命令格式错误")
+            msg = f"{place_name}现在{place.count}人 (更新于{place.update_time.strftime('%H:%M')}，更新数据请使用“/{place_name}3”或“/{place_shortname}+1”。)"
+        await bot.send(event=event, message=msg)
 
-    await bot.send(event=event, message="更新成功，现活动室人数为" + str(hds.count))
+    @update_place.handle()
+    async def update_count(bot: Bot, event: Event, matcher: Matcher, args: Message = CommandArg()):
+        plain_text = args.extract_plain_text()
+        try:
+            if plain_text[0] == '+':
+                n1 = int(plain_text[1:])
+                place.plus(n1)
+            elif plain_text[0] == '-':
+                n1 = int(plain_text[1:])
+                place.plus(-n1)
+            else:
+                n1 = int(plain_text)
+                place.set(n1)
+        except ValueError:
+            await matcher.finish(message="命令格式错误")
+
+        await bot.send(event=event, message=f"更新成功，现{place_name}人数为{place.count}")
