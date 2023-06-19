040328eb1c796393a30915ce7fb1e4467f684417
ssttkkl <huang.wen.long@hotmail.com>
2023-06-19 19:55:06 +0800

    删除ff14插件与maimai插件

--- plugins/ff14/README.md
+++
@@ -1,4 +0,0 @@
-ff14
-======
-
-Forked from [he0119/CoolQBot](https://github.com/he0119/CoolQBot)
--- plugins/ff14/utils/typing.py
+++
@@ -1,4 +0,0 @@
-from collections.abc import Callable, Sequence
-from typing import Union
-
-Expression_T = Union[str, Sequence[str], Callable[..., str]]
