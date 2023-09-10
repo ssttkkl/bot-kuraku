fa631ce6bf29516d6340b19dd45a34749ca4072b
ssttkkl <huang.wen.long@hotmail.com>
2023-09-10 23:51:48 +0800

    超能力麻将使用图片发送

--- plugins/nonebot_plugin_super_ability_mahjong/__init__.py
+++ plugins/nonebot_plugin_super_ability_mahjong/__init__.py
@@ -1,3 +1,8 @@
+from nonebot import require
+
+require("nonebot_plugin_htmlrender")
+require("nonebot_plugin_saa")
+
 import json
 import random
 
@@ -6,10 +11,14 @@ from nonebot.adapters import Bot, Event
 from nonebot.adapters import Message
 from nonebot.params import CommandArg
 
+from nonebot_plugin_htmlrender import md_to_pic
+from nonebot_plugin_saa import MessageFactory, Image
+
 data = []
 
 with open('./superMahjongData.json', 'r') as json_file:
     data = json.load(json_file)
+
 
 @on_command("sid", aliases={"查牌", "id"}, priority=5).handle()
 async def select_by_id(bot: Bot, event: Event, args: Message = CommandArg()):
@@ -19,6 +28,7 @@ async def select_by_id(bot: Bot, event: Event, args: Message = CommandArg()):
             await bot.send(event=event, message=f"{element['id']}. {element['name']}: {element['details']}")
             break
 
+
 @on_command("sname", aliases={"查牌名", "sn"}, priority=5).handle()
 async def select_by_name(bot: Bot, event: Event, args: Message = CommandArg()):
     target_name = args.extract_plain_text()
@@ -26,6 +36,7 @@ async def select_by_name(bot: Bot, event: Event, args: Message = CommandArg()):
         if element["name"] == target_name:
             await bot.send(event=event, message=f"{element['id']}. {element['name']}: {element['details']}")
             break
+
 
 @on_command("新建超能力对局", aliases={"随机超能力牌组", "sam"}, priority=5).handle()
 async def rand_12_cards(bot: Bot, event: Event):
@@ -44,13 +55,31 @@ async def rand_12_cards(bot: Bot, event: Event):
             else:
                 north.append(element)
 
-    output = f"""
-    东家抽到的牌组为：{east[0]['id']}. {east[0]['name']}：{east[0]['details']}\n{east[1]['id']}. {east[1]['name']}：{east[1]['details']}\n{east[2]['id']}. {east[2]['name']}：{east[2]['details']}\n
-    南家抽到的牌组为：{south[0]['id']}. {south[0]['name']}：{south[0]['details']}\n{south[1]['id']}. {south[1]['name']}：{south[1]['details']}\n{south[2]['id']}. {south[2]['name']}：{south[2]['details']}\n
-    西家抽到的牌组为：{west[0]['id']}. {west[0]['name']}：{west[0]['details']}\n{west[1]['id']}. {west[1]['name']}：{west[1]['details']}\n{west[2]['id']}. {west[2]['name']}：{west[2]['details']}\n
-    北家抽到的牌组为：{north[0]['id']}. {north[0]['name']}：{north[0]['details']}\n{north[1]['id']}. {north[1]['name']}：{north[1]['details']}\n{north[2]['id']}. {north[2]['name']}：{north[2]['details']}。
-    请各家在抽到的牌组中选择一张牌作为本局技能。
-    """
+    output = f"## 东家抽到的牌组为：" \
+             "\n" \
+             f"- **{east[0]['id']}. {east[0]['name']}**：{east[0]['details']}\n" \
+             f"- **{east[1]['id']}. {east[1]['name']}**：{east[1]['details']}\n" \
+             f"- **{east[2]['id']}. {east[2]['name']}**：{east[2]['details']}\n" \
+             "\n" \
+             f"## 南家抽到的牌组为：" \
+             "\n" \
+             f"- **{south[0]['id']}. {south[0]['name']}**：{south[0]['details']}\n" \
+             f"- **{south[1]['id']}. {south[1]['name']}**：{south[1]['details']}\n" \
+             f"- **{south[2]['id']}. {south[2]['name']}**：{south[2]['details']}\n" \
+             "\n" \
+             f"## 西家抽到的牌组为：" \
+             "\n" \
+             f"- **{west[0]['id']}. {west[0]['name']}**：{west[0]['details']}\n" \
+             f"- **{west[1]['id']}. {west[1]['name']}**：{west[1]['details']}\n" \
+             f"- **{west[2]['id']}. {west[2]['name']}**：{west[2]['details']}\n" \
+             "\n" \
+             f"## 北家抽到的牌组为：" \
+             "\n" \
+             f"- **{north[0]['id']}. {north[0]['name']}**：{north[0]['details']}\n" \
+             f"- **{north[1]['id']}. {north[1]['name']}**：{north[1]['details']}\n" \
+             f"- **{north[2]['id']}. {north[2]['name']}**：{north[2]['details']}。\n" \
+             "\n" \
+             f"请各家在抽到的牌组中选择一张牌作为本局技能。"
 
-    await bot.send(event=event, message=output)
-
+    image = await md_to_pic(output)
+    await MessageFactory(Image(image)).send()
