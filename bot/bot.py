2d59334fa5c4707d20d283b91828d6c75fffa4ec
ssttkkl <huang.wen.long@hotmail.com>
2025-01-23 00:15:12 +0800

    注册qq adapter

--- bot/bot.py
+++ bot/bot.py
@@ -8,10 +8,12 @@ nonebot.init()
 app = nonebot.get_asgi()
 
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
+from nonebot.adapters.qq import Adapter as QQAdapter
 
 # 注册adapter
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
+driver.register_adapter(QQAdapter)
 
 # 加载插件
 nonebot.load_from_toml("pyproject.toml")
